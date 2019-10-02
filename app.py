f0a1d8b33bb69f1d68f89aeddfedebec480a2a91
romain-ngo <ngo.romain@gmail.com>
2019-10-02 22:36:52 +0200

    Implemented blueprint for user route

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 from flask import Flask
+from routes.user_routes import userRoute
 
 app = Flask(__name__)
+app.register_blueprint(userRoute)
---
+++ routes/user_routes.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+
+userRoute = Blueprint('userRoute', __name__)
+
+
+@userRoute.route('/users')
+def get_users():
+    return 'users'
